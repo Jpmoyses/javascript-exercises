64be546d43b9431cfeff0370afb735e8b8c4b106
jpmoyses <jpmoyses00@gmail.com>
2024-11-01 16:13:21 -0300

    Completed exercises 5-7

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function(num1 = 0, num2 = 0) {
+    let tempNum = 0;
+    let finalNumber = 0;
 
+    // test for errors
+    if (num1 < 0 || num2 < 0 || !Number.isInteger(num1)
+        || !Number.isInteger(num2)){
+        return 'ERROR';
+    }
+    // changes order in case num2 is lower
+    else if (num1 > num2){
+        tempNum = num1;
+        num1 = num2;
+        num2 = tempNum;
+    }
+
+    // main loop for the function
+    for(let i = num1; i <= num2; i++){
+        finalNumber += i;
+    }
+    return finalNumber;
 };
 
 // Do not edit below this line
