f09a31e58ad06ca553cb82196636350ba61aeeb1
jpmoyses <jpmoyses00@gmail.com>
2024-11-20 16:29:29 -0300

    Completed exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(...numbers) {
+	return numbers.reduce((total, currentNumber) => total + currentNumber, 0);
 };
 
-const subtract = function() {
-	
+const subtract = function(...numbers) {
+  return numbers.reduce((total, currentNumber) => total - currentNumber);
 };
 
-const sum = function() {
-	
+const sum = function(...numbers) {
+  let totalSum = 0;
+	numbers.forEach((item) => totalSum += item.reduce((total, current) => total + current, 0))
+  return totalSum;
 };
 
-const multiply = function() {
-
+const multiply = function(...numbers) {
+  let totalSum = 0;
+	numbers.forEach((item) => totalSum += item.reduce((total, current) => total * current))
+  return totalSum;
 };
 
-const power = function() {
-	
+const power = function(...numbers) {
+	let power = numbers[1];
+  let total = 1;
+  for(let i = 0; i < power; i++){
+    total *= numbers[0];
+  }
+  return total;
 };
 
-const factorial = function() {
-	
+const factorial = function(...numbers) {
+  if (numbers[0] == 0){return 1}
+  let total = 1;
+	for(i = numbers[0]; i > 0; i--) total *= i;
+  return total;
 };
 
 // Do not edit below this line
