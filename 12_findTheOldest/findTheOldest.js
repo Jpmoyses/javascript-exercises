806c33e23cf7e425a68af32304adb0633a1f8c56
jpmoyses <jpmoyses00@gmail.com>
2024-11-21 14:13:45 -0300

    Completed exercises 11 and 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,12 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    let sorted = people.sort((a, b) => {
+        if (a.yearOfDeath === undefined || b.yearOfDeath === undefined) {
+            let date = new Date();
+            a.yearOfDeath === undefined ? a.yearOfDeath = date.getFullYear() : b.yearOfDeath = date.getFullYear();
+        }
+        return (a.yearOfDeath - a.yearOfBirth) > (b.yearOfDeath - b.yearOfBirth) ? -1 : 1;
+    })
+    return sorted[0];
 };
 
 // Do not edit below this line
