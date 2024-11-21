e26b14357442a691b10a46004b27028d120ab0db
jpmoyses <jpmoyses00@gmail.com>
2024-11-21 13:46:12 -0300

    Completed exercise 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,21 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    let num1 = 0;
+    let num2 = 1;
+    let total = 1;
+    for(let i = 0; i < num - 1; i++){
+        if(i % 2 == 0){
+            num2 = total;
+            total += num1;
+        }
+        else{
+            num1 = total;
+            total += num2
+        };
+    }
+    if(num == 0)return 0;
+    else if(num < 0) return "OOPS";
+    
+    return total;
 };
 
 // Do not edit below this line
