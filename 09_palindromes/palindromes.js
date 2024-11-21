37a738d3069c3dc5a2f4b340c0e3147ab452f36d
jpmoyses <jpmoyses00@gmail.com>
2024-11-21 13:31:17 -0300

    Completed Exercise 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function () {
-
+const palindromes = function (words = '') {
+    words = words.replaceAll(' ', '').toLowerCase();
+    words = words.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"")
+    let wordArray = words.split('');
+    let wordArrayReverse = [...wordArray];
+    wordArrayReverse.reverse();
+    console.log(wordArray, wordArrayReverse);
+    for(let i = 0; i < wordArray.length; i++){
+        if (wordArray[i] !== wordArrayReverse[i]){
+            return false;
+        }
+    }
+    return true;
 };
 
 // Do not edit below this line
